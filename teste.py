7229b9bb0667ee12b1fd6fe89ccb03ae786a6012
Matheus Rodrigues <rodrigues_20191@outlook.com>
2025-03-15 19:52:49 -0300

    criando segunda função print_ola_mundo()

--- teste.py
+++ teste.py
@@ -1,2 +1,8 @@
-while True():
-    print('olá mundo')
+def printa_ola_mundo():
+    while True():
+        print('olá mundo')
+
+
+def printa_ola_mundo1():
+    while True():
+        print('olá mundo')
